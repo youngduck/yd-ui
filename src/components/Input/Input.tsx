621a3c5e66d81367f93aa9a934f4870b0ba5cd54
youngduck <dudejr5839@gmail.com>
2025-09-15 23:36:08 +0900

    feat: input컴포넌트 focus 색상제거

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -18,7 +18,7 @@ const wrapperVariants = cva('relative', {
   },
 })
 
-const inputVariants = cva('yds-input-typography h-12 rounded-[8px] border-2 pr-3', {
+const inputVariants = cva('yds-input-typography h-12 rounded-[8px] border-2 pr-3 focus:outline-none', {
   variants: {
     size: {
       sm: 'w-[200px]',
@@ -29,9 +29,9 @@ const inputVariants = cva('yds-input-typography h-12 rounded-[8px] border-2 pr-3
     color: {
       white: 'border-white bg-transparent text-white placeholder-white',
       'primary-400':
-        'border-yellow-400 bg-transparent text-primary-400 placeholder-primary-400 focus:border-yellow-500',
+        'border-yellow-400 bg-transparent text-primary-400 placeholder-primary-400',
       'primary-100':
-        'border-yellow-100 bg-transparent text-primary-100 placeholder-primary-100 focus:border-yellow-200',
+        'border-yellow-100 bg-transparent text-primary-100 placeholder-primary-100',
     },
     variant: {
       search: 'pl-10', // 아이콘 공간
@@ -55,17 +55,16 @@ export type InputColor = VariantProps<typeof inputVariants>['color']
 export type InputVariant = VariantProps<typeof inputVariants>['variant']
 
 export type InputProps = {
-  className?: string
 } & Omit<React.InputHTMLAttributes<HTMLInputElement>, 'size' | 'color'> &
   VariantProps<typeof inputVariants>
 
-export function Input({ size, color, variant, className = '', disabled, ...props }: InputProps) {
+export function Input({ size, color, variant, disabled, ...props }: InputProps) {
   return (
     <div className={wrapperVariants({ color })}>
       {variant === 'search' && (
         <Search aria-hidden className="absolute left-3 top-1/2 -translate-y-1/2 w-6 h-6 text-current" />
       )}
-      <input className={inputVariants({ size, color, variant, disabled, className })} disabled={disabled} {...props} />
+      <input className={inputVariants({ size, color, variant, disabled })} disabled={disabled} {...props} />
     </div>
   )
 }
