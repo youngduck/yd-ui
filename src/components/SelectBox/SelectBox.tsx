f29c17d9cd128c4ca280d43faa64cf03311b6a53
youngduck <dudejr5839@naver.com>
2025-09-22 20:04:14 +0900

    feat: 옵션 세로길이 자동스크롤

--- src/components/SelectBox/SelectBox.tsx
+++ src/components/SelectBox/SelectBox.tsx
@@ -42,7 +42,7 @@ export function SelectBox({ size, selectBoxHook }: SelectBoxProps) {
   return (
     <div className={wrapperVariants({ size })} ref={containerRef}>
       <div
-        className="text-primary-100 border-primary-100 text-yds-b1 absolute top-0 left-0 flex h-full w-full cursor-pointer items-center justify-between rounded-lg border-2 p-3"
+        className="text-primary-100 border-primary-100 text-yds-b1 flex h-full w-full cursor-pointer items-center justify-between rounded-lg border-2 p-3"
         onClick={handleToggle}
       >
         {!hasOption && '선택'}
@@ -50,7 +50,7 @@ export function SelectBox({ size, selectBoxHook }: SelectBoxProps) {
         <ChevronDown className="text-primary-100 transition-transform duration-300" />
       </div>
       {isOpen && (
-        <div className="bg-background-secondary border-primary-100 absolute top-14 left-0 flex h-auto w-full flex-col gap-2 rounded-lg border-2 p-3">
+        <div className="bg-background-secondary border-primary-100 z-select-box-dropdown absolute top-14 left-0 flex w-full flex-col gap-2 rounded-lg border-2 p-3">
           {search && (
             <div className="border-primary-100 flex h-[40px] items-center border-y-2">
               <Search className="text-primary-100" size={20} />
@@ -63,18 +63,20 @@ export function SelectBox({ size, selectBoxHook }: SelectBoxProps) {
               />
             </div>
           )}
-          {options
-            .filter(option => option.value.includes(searchValue))
-            .map(option => (
-              <div
-                key={option.label}
-                className="text-yds-c1m hover:bg-background-primary flex cursor-pointer items-center justify-between text-white"
-                onClick={() => handleClickOption(option)}
-              >
-                {option.value}
-                {selectedOption.value === option.value && <Check className="text-primary-100" size={20} />}
-              </div>
-            ))}
+          <div className="scrollbar-hide flex h-auto max-h-[200px] flex-col gap-2 overflow-y-scroll">
+            {options
+              .filter(option => option.value.includes(searchValue))
+              .map(option => (
+                <div
+                  key={option.label}
+                  className="text-yds-c1m hover:bg-background-primary flex cursor-pointer items-center justify-between text-white"
+                  onClick={() => handleClickOption(option)}
+                >
+                  {option.value}
+                  {selectedOption.value === option.value && <Check className="text-primary-100" size={20} />}
+                </div>
+              ))}
+          </div>
         </div>
       )}
     </div>
